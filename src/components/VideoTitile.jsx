b8ee7f95e285dfc5ffcab31a764f06d9c67fb9d9
YUVRAJ SINGH <yyuvrajssingh2001@gmail.com>
2024-01-02 22:50:46 +0530

    modified

--- src/components/VideoTitile.jsx
+++ src/components/VideoTitile.jsx
@@ -7,7 +7,7 @@ const curlang=useSelector(store=>store.config.lang)
 
         <div className="w-screen aspect-video  pt-[20%] px-6 md:px-24 absolute text-white bg-gradient-to-r from-black">
             <h1 className="text-2xl md:text-6xl w-1/2 font-bold">{title}</h1>
-            <h1 className="hidden md:inline-block p-6 text-lg w-2/4">{overview}</h1>
+            <h1 className="hidden  p-6 text-lg w-2/4">{overview}</h1>
             
             <div className="flex">
                 <button className="bg-white rounded-lg hidden md:inline-block  p-4 px-12 text-xl text-black hover:bg-opacity-75">{lang[curlang].play}</button>
